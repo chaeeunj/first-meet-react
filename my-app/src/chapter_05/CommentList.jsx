7e7d4e7ae43529c44e9c59d19e32dc2476f5502b
chaeeunj <official2930@naver.com>
2022-07-06 05:14:38 +0900

    처음 만난 리액트v2 5-5. 댓글 데이터 객체로 만들기

--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Comment from './Comment';
 
+const comments = [
+  {
+    name: '김민수',
+    comment: '안녕하세요, 민수입니다.',
+  },
+  {
+    name: '윤지민',
+    comment: '지금은 2022년 입니다.',
+  },
+  {
+    name: '박영철',
+    comment: '날씨가 더워요.',
+  },
+];
+
 function CommentList(props) {
   return (
     <div>
-      <Comment name={'김민수'} comment={'안녕하세요, 민수입니다.'} />
-      <Comment name={'윤지민'} comment={'안녕하세요, 지민입니다.'} />
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
     </div>
   );
 }
