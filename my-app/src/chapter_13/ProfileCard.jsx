8443e8ab0cb10736ab2acabcec8652ad4de59887
chaeeunj <official2930@naver.com>
2022-07-21 04:20:22 +0900

    처음 만난 리액트v2. 13-2 Card 컴포넌트 만들기

---
+++ my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from './Card';
+
+function ProfileCard(props) {
+  return (
+    <Card title="Chan J" backgroundColor="#4ea04e">
+      <p>안녕하세요, chan 입니다.</p>
+      <p>저는 리액트를 공부하는 중입니다.</p>
+    </Card>
+  );
+}
+
+export default ProfileCard;
